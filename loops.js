69927a5c995de4aad4d1d91a447d111291961efc
adithya <adithyayegiti93@gmail.com>
2026-02-28 18:51:11 +0530

    first oush

---
+++ loops.js
@@ -0,0 +1,17 @@
+let j=5;
+// for (i = 1; i <= 10; i++) {
+//     //5X1=5
+
+//     result=j*i
+//     console.log(`${j}X${i}=${result}`);
+//     console.log("5 X " + i + "=" + i*5);
+
+
+
+// }
+//Array 
+const arr=[1,2,3,4,5,"a"];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
