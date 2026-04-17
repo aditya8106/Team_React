c3d3f43386a0a81553b6a9513e2faec754c9301e
adithya <adithyayegiti93@gmail.com>
2026-04-17 19:24:26 +0530

    loop

--- loops.js
+++ loops.js
@@ -1,5 +1,4 @@
-let j=5;
-// for (i = 1; i <= 10; i++) {
+// = 1; i <= 10; i++) {
 //     //5X1=5
 
 //     result=j*i
@@ -10,8 +9,19 @@ let j=5;
 
 // }
 //Array 
-const arr=[1,2,3,4,5,"a"];
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
+const arr=[1,2,3,4,5,5,5];
+let obj={}
+function countOccurences(arr){
+    for(let i=0;i<arr.length;i++){
+        if(obj[arr[i]]){    
+            obj[arr[i]]+=1
+        } else {
+            obj[arr[i]]=1
+        }
+    }
+    return obj;
 }
+console.log(countOccurences(arr));
 
+
+console.log(obj[arr[5]]);
