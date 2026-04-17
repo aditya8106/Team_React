99291bb58896723a5ba6294a0c6ae807bea0c589
adithya <adithyayegiti93@gmail.com>
2026-04-17 19:44:51 +0530

    const

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 console.log("Hello World");
-let a=10;
-let b=20;
-let c=a+b;
+let a1=10;
+let b1=20;
+let c=a1+b1;
 console.log(c);
     let num = 3;
     console.log(num+4);
@@ -11,6 +11,19 @@ console.log(name);
 name="Doe";
 console.log(name);
 if (true){
-    var name="Jane";
+    var name="Jane"; /// function scope
 }
 console.log(name);
+
+if (true){
+    let name="nandu"; /// block scope
+}
+console.log(name);
+
+const pi=3.14;
+console.log(pi);
+// pi=3.1415; // error
+const  a=5;
+const  b=10;
+ [a,b]=[b,a];
+console.log(a,b);
