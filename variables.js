498672e6cb0288340c331a5f69ff76714172c116
adithya <adithyayegiti93@gmail.com>
2026-04-17 19:40:35 +0530

    variable

---
+++ variables.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
+    let num = 3;
+    console.log(num+4);
+
+var name="John";
+console.log(name);
+name="Doe";
+console.log(name);
+if (true){
+    var name="Jane";
+}
+console.log(name);
